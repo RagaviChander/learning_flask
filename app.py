49f60e81056e401662c4d122277ce07dfbab29b8
Ragavi Chander <ragavi.chander@gmail.com>
2024-03-27 23:30:05 -0500

    New changes

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "hello world, index"
+
+@app.route("/ragavi")
+def ragavi():
+    n1 = 3
+    n2 = 6
+    sum= n1+n2
+    return str(sum)
+
+if __name__ == "__main__":
+    app.run(debug=True)
