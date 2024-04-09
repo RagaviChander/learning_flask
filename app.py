aab8d42358d3e644c9abf37a0f7b8c0051311a28
SnehPat21 <sneh-patel@uiowa.edu>
2024-04-08 19:35:08 -0500

    added about links

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "hello world, index"
+    return render_template("index.html")
 
-@app.route("/ragavi")
-def ragavi():
-    n1 = 3
-    n2 = 6
-    sum= n1+n2
-    return str(sum)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 4444)), debug=True
+    )
